3d9ed38c9c1fa239837719c77fdef07835c51009
Vikash Singh <Vikash.Singh2@Evalueserve.com>
2025-11-11 15:18:24 +0530

    DesignNewStrategy completed

--- src/sidebar/DesignNewStrategy.js
+++ src/sidebar/DesignNewStrategy.js
@@ -2,24 +2,85 @@ import "./DesignNewStrategy.css";
 import DialogBox from "./DialogBox";
 import RadioButton from "./RadioButton";
 import { useState } from "react";
+import ExcelTable from "./ExcelTable";
+import ExcelTransposeTable from "./ExcelTransposeTable";
 
 function App() {
   const [isDialogOpen, setDialogOpen] = useState(false);
+  const sampleData = [
+    {
+      ISIN: "",
+      Ticker: "",
+      RIC: "",
+      Currency: "",
+      DividendCurrency: "",
+      ExchangeCountry: "",
+      DataSource: "",
+      Actions: "↓  👁",
+    },
+    {
+      ISIN: "",
+      Ticker: "",
+      RIC: "",
+      Currency: "",
+      DividendCurrency: "",
+      ExchangeCountry: "",
+      DataSource: "",
+      Actions: "↓  👁",
+    },
+    {
+      ISIN: "",
+      Ticker: "",
+      RIC: "",
+      Currency: "",
+      DividendCurrency: "",
+      ExchangeCountry: "",
+      DataSource: "",
+      Actions: "↓  👁",
+    },
+    {
+      ISIN: "",
+      Ticker: "",
+      RIC: "",
+      Currency: "",
+      DividendCurrency: "",
+      ExchangeCountry: "",
+      DataSource: "",
+      Actions: "↓  👁",
+    },
+  ];
+  const sampleTransData = [
+    { Currency: "", Static: "", Action: "" },
+    { Currency: "", Static: "", Action: "" },
+    { Currency: "", Static: "", Action: "" },
+    { Currency: "", Static: "", Action: "" },
+    { Currency: "", Static: "", Action: "" },
+    { Currency: "", Static: "", Action: "" },
+  ];
 
   return (
     <div className="container">
       {/* Section 1 */}
       <div className="section section1">
-          <RadioButton options={["Replicate Existing Strategy", "Create New Strategy"]} labelName={"strategyType"}></RadioButton>
+        <RadioButton
+          options={["Replicate Existing Strategy", "Create New Strategy"]}
+          labelName={"strategyType"}
+        ></RadioButton>
       </div>
 
       {/* Section 2 */}
       <div className="section">
         <div className="section2">
-          <RadioButton options={["Index", "Basket", "TWAPS/VWAPS"]} labelName={"assetType1"}></RadioButton>
+          <RadioButton
+            options={["Index", "Basket", "TWAPS/VWAPS"]}
+            labelName={"assetType1"}
+          ></RadioButton>
         </div>
         <div className="section2">
-          <RadioButton options={["Eq", "Eq Drv", "Rates", "Comm", "Multi Asset"]} labelName={"assetType2"}></RadioButton>
+          <RadioButton
+            options={["Eq", "Eq Drv", "Rates", "Comm", "Multi Asset"]}
+            labelName={"assetType2"}
+          ></RadioButton>
         </div>
       </div>
 
@@ -53,7 +114,7 @@ function App() {
           <input
             type="textarea"
             placeholder="Basic Description"
-            style={{ height: "60px", width: "270px" }}
+            style={{ height: "60px", width: "370px" }}
           />
           <br />
           <label style={{ marginTop: "10px" }}>
@@ -61,7 +122,9 @@ function App() {
           </label>
         </div>
         <div className="col">
-          <button className="btn" onClick={() => setDialogOpen(true)}>UI Editor</button>
+          <button className="btn" onClick={() => setDialogOpen(true)}>
+            UI Editor
+          </button>
           <DialogBox
             isOpen={isDialogOpen}
             onClose={() => setDialogOpen(false)}
@@ -71,6 +134,55 @@ function App() {
           </DialogBox>
         </div>
       </div>
+      <div className="section section4">
+        <div className="row">
+          <h4>Underlying Instrument</h4>
+          <button className="btn" onClick={() => setDialogOpen(true)}>
+            Ticker Lookup
+          </button>
+          <DialogBox
+            isOpen={isDialogOpen}
+            onClose={() => setDialogOpen(false)}
+            title="Hello Vikash!"
+          >
+            <p>This is a simple dialog box in React.</p>
+          </DialogBox>
+        </div>
+        <div style={{ width: "100%" }}>
+          <div style={{ width: "600px", height: "auto", margin: "20px 50px" }}>
+            <ExcelTable data={sampleData} />
+          </div>
+        </div>
+      </div>
+      <div className="section section5">
+        <h4>Parameters</h4>
+        {/* <div style={{width: "700px"}}> */}
+        <ExcelTransposeTable data={sampleTransData} />
+        {/* </div> */}
+        <button className="btn" style={{ width: "150px" }}>
+          Edit and Update
+        </button>
+      </div>
+      <div className="section section6">
+        <h4>Backtest Parameters</h4>
+        <div className="backtest-grid">
+          <div className="row">
+            <label>From Date:</label>
+            <input type="date" />
+            <label>Data Sufficiency Check:</label>
+            <input type="text" placeholder="check" />
+          </div>
+          <div className="row">
+            <label>To Date:</label>
+            <input type="date" />
+            <label>Switch Date for Data Source:</label>
+            <input type="date" />
+          </div>
+        </div>
+      </div>
+        <div className="generate-btn">
+          <button className="btn">Generate Backtest</button>
+        </div>
     </div>
   );
 }
